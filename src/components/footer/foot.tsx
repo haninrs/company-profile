d608463e7a4476bdccf2b0cb03d89760e587e8be
haninrs <haninrry@gmail.com>
2024-03-24 23:17:14 +0700

    fix

--- src/components/footer/foot.tsx
+++ src/components/footer/foot.tsx
@@ -5,30 +5,28 @@ import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
 
 export default function FooterPage() {
   return (
-    <div>
-      <footer className="footer footer-center p-10 bg-slate-900 rounded">
-        <nav className="grid grid-flow-col gap-4">
-          <ButtonLinks href="/about">About</ButtonLinks>
-          <ButtonLinks href="/service">Service</ButtonLinks>
-          <ButtonLinks href="/team">Teams</ButtonLinks>
-        </nav>
-        <nav>
-          <div className="grid grid-flow-col gap-4 ">
-            <Link href="https://twitter.com/elonmusk" target="_blank">
-              <FaTwitter className="fill-slate-50 w-6 h-6" />
-            </Link>
-            <Link href="https://www.youtube.com/@AdiHidayatOfficial" target="_blank">
-              <FaYoutube className="fill-slate-50 w-6 h-6" />
-            </Link>
-            <Link href="https://www.facebook.com/" target="_blank">
-              <FaFacebook className="fill-slate-50 w-6 h-6" />
-            </Link>
-          </div>
-        </nav>
-        <aside>
-          <p className="text-slate-50">Copyright © 2024 - All right reserved by GAMESTART Industries Ltd</p>
-        </aside>
-      </footer>
-    </div>
+    <footer className="footer footer-center p-10 bg-slate-900 rounded">
+      <nav className="grid grid-flow-col gap-4">
+        <ButtonLinks href="/about">About</ButtonLinks>
+        <ButtonLinks href="/service">Service</ButtonLinks>
+        <ButtonLinks href="/team">Teams</ButtonLinks>
+      </nav>
+      <nav>
+        <div className="grid grid-flow-col gap-4 ">
+          <Link href="https://twitter.com/elonmusk" target="_blank">
+            <FaTwitter className="fill-slate-50 w-6 h-6" />
+          </Link>
+          <Link href="https://www.youtube.com/@AdiHidayatOfficial" target="_blank">
+            <FaYoutube className="fill-slate-50 w-6 h-6" />
+          </Link>
+          <Link href="https://www.facebook.com/" target="_blank">
+            <FaFacebook className="fill-slate-50 w-6 h-6" />
+          </Link>
+        </div>
+      </nav>
+      <aside>
+        <p className="text-slate-50">Copyright © 2024 - All right reserved by GAMESTART Industries Ltd</p>
+      </aside>
+    </footer>
   );
 }
