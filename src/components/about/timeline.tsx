548d610982c3c08da936510f69c7259479e88041
haninrs <haninrry@gmail.com>
2024-03-24 15:57:42 +0700

    second commit

--- src/components/about/timeline.tsx
+++ src/components/about/timeline.tsx
@@ -19,10 +19,10 @@ export default function TimelineSec() {
                 unde odit vitae minima! Iste cum sint amet officiis.
               </span>
             </div>
-            <hr />
+            <hr className="bg-slate-50" />
           </li>
           <li>
-            <hr />
+            <hr className="bg-slate-50" />
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
@@ -33,10 +33,10 @@ export default function TimelineSec() {
               <div className="text-lg font-black">Patnered with Malaysia</div>
               <span className="text-pretty align-text-top hidden md:inline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit excepturi ullam quaerat fuga animi? Nobis officiis excepturi ut.</span>
             </div>
-            <hr />
+            <hr className="bg-slate-50" />
           </li>
           <li>
-            <hr />
+            <hr className="bg-slate-50" />
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
@@ -49,10 +49,10 @@ export default function TimelineSec() {
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident quasi voluptatum sit magnam tenetur necessitatibus quis voluptates modi repellendus nostrum!
               </span>
             </div>
-            <hr />
+            <hr className="bg-slate-50" />
           </li>
           <li>
-            <hr />
+            <hr className="bg-slate-50" />
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
@@ -66,10 +66,10 @@ export default function TimelineSec() {
                 dolores illo.
               </span>
             </div>
-            <hr />
+            <hr className="bg-slate-50" />
           </li>
           <li>
-            <hr />
+            <hr className="bg-slate-50" />
             <div className="timeline-middle">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" />
--- src/components/vidioMd.tsx
+++ src/components/vidioMd.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-export default function VidioMd() {
+const VidioMd: React.FC = () => {
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) return;
+
+    const intersectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            // Load the video when it enters the viewport
+            iframe.src = iframe.dataset.src!;
+            intersectionObserver.unobserve(entry.target);
+          }
+        });
+      },
+      { rootMargin: "0px", threshold: 0.1 }
+    );
+
+    intersectionObserver.observe(iframe);
+
+    // Cleanup
+    return () => {
+      intersectionObserver.disconnect();
+    };
+  }, []);
+
   return (
-    <div>
+    <div className="aspect-w-16 aspect-h-9">
       <iframe
         className="lg:w-[560px] lg:h-[315px] m-auto md:w-[374] md:h-[210] rounded-md "
         width="280"
@@ -11,9 +38,9 @@ export default function VidioMd() {
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen>
-        
-        </iframe>
+        allowFullScreen></iframe>
     </div>
   );
-}
+};
+
+export default VidioMd;
