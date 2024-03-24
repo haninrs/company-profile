dd30aea4e88f3c72eff56e911e3d464702a5cc3a
haninrs <haninrry@gmail.com>
2024-03-24 23:18:17 +0700

    vidio

--- src/components/team/page.tsx
+++ src/components/team/page.tsx
@@ -10,7 +10,7 @@ export interface ITeam {
 
 export const TeamCard: React.FC<ITeam> = ({ firstName, lastName, img, phone }: ITeam) => {
   return (
-    <div className="w-full max-w-sm bg-gradient-to-b from-teal-950  to-transparent border border-slate-50 rounded-lg shadow">
+    <div className="w-full max-w-sm bg-gradient-to-b from-teal-950  to-transparent border px-12 border-slate-50 rounded-lg shadow">
       <div className="flex flex-col items-center justify-center py-10">
         <Image className="w-24 h-24 mb-3 rounded-full shadow-lg border border-slate-50" width={70} height={70} src={`${img}`} alt={`${firstName}`} />
         <h5 className="pb-1 text-xl font-medium text-slate-50 ">
